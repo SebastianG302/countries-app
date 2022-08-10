bf38df94f464b1f28485a08d3e195b0c339ac3d5
SebastianG302 <johan.guerrero@pi.edu.co>
2022-08-10 17:54:54 -0500

    creation of module routing and routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { PaisModule } from './pais/pais.module';
@@ -12,7 +13,8 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     SharedModule,
-    PaisModule
+    PaisModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
