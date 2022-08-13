299ca820140628eae100aa5ac58e7bc1543f476d
SebastianG302 <johan.guerrero@pi.edu.co>
2022-08-13 16:44:26 -0500

    error handling and pais interface

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
   styleUrls: ['./por-pais.component.css']
 })
-export class PorPaisComponent implements OnInit {
+export class PorPaisComponent   {
 
-  constructor() { }
+  termino: string = "";
+  hayError: boolean = false
 
-  ngOnInit(): void {
+  constructor(private paisService: PaisService) { }
+
+
+  buscar(){
+    this.hayError = false
+    console.log(this.termino);
+    this.paisService.buscarPais( this.termino )
+    .subscribe(
+      (resp) => {
+        console.log(resp)
+      },
+      (err) => {
+        this.hayError = true;
+      }
+    )
   }
-
 }
