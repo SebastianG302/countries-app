ad4ae64f347655665f27e969883b14d23f0d8aee
SebastianG302 <johan.guerrero@pi.edu.co>
2022-08-26 18:24:12 -0500

    update ver-pais component

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,14 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { switchMap, tap } from 'rxjs/operators';
 
 import { PaisService } from '../../services/pais.service';
-import { Country } from '../../interfaces/pais-interface';
 
 @Component({
   selector: 'app-ver-pais',
   templateUrl: './ver-pais.component.html',
   styles: [
+    '.badge{ margin: 3px; }'
   ]
 })
 export class VerPaisComponent implements OnInit {
